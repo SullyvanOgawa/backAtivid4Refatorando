8c43267946c4c2970d10a628372fb1274c347a09
Sullyvan Ogawa <sullyvanogawa@gmail.com>
2026-09-02 20:47:13 -0300

    Conectando com o frontEnd

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 import express from 'express';
 import routerCategory from './routes/routerCategory.js';
 import routerProject from './routes/routerProject.js';
+import cors from 'cors';
 
 const app = express();
+app.use(cors({
+    origin: 'http://localhost:5173'
+}));
 
 app.use(express.json());
 
